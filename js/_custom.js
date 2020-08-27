95653880d4e8da90e2ec1fcc32f73828c182f44e
Maksim Koshman <maksimkoshman090@gmail.com>
2020-08-27 09:00:09 +0300

    add changes

--- js/_custom.js
+++ js/_custom.js
@@ -1,21 +1,4 @@
 $(document).ready(function () {
-  // Reviews Slider
-  $("#reviewsSlider").slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: "#reviewsNav",
-    arrows: false,
-  });
-
-  $("#reviewsNav").slick({
-    infinite: true,
-    arrows: false,
-    slidesToShow: 2,
-    asNavFor: "#reviewsSlider",
-    focusOnSelect: true,
-  });
-
   // Videos Player
   $(".videos-video").on("click", function () {
     $(this).children(".videos-play").hide();
