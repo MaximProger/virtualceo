71f2d6ea6c2e7ec0ba8cddca641d1e2c26fe729e
Maksim Koshman <maksimkoshman090@gmail.com>
2020-09-03 18:07:21 +0300

    add videos

--- js/_custom.js
+++ js/_custom.js
@@ -2,5 +2,9 @@ $(document).ready(function () {
   // Videos Player
   $(".videos-video").on("click", function () {
     $(this).children(".videos-play").hide();
+    $(this).children(".videos-img").addClass("active");
+    let currentSrc =
+      $(this).children(".videos-img").attr("src") + "?autoplay=1";
+    $(this).children(".videos-img").attr("src", currentSrc);
   });
 });
